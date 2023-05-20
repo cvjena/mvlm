3d3d00c4d435930994c639170bb2e0d40a6c8ef4
Tim Büchner <tim.buechner@uni-jena.de>
2023-05-20 13:08:17 +0200

    update render server

--- 3DMD_server.py
+++ 3DMD_server.py
@@ -1,5 +1,4 @@
 import fastapi
-import pandas as pd
 import pydantic
 
 import deepmvlm
@@ -9,7 +8,9 @@ from parse_config import ConfigParser
 
 class Item(pydantic.BaseModel):
     path_in: str
-    path_out: str
+
+class Landmarks(pydantic.BaseModel):
+    landmarks: list[list[float]]
 
 app = fastapi.FastAPI()
 
@@ -22,8 +23,6 @@ dm = deepmvlm.DeepMVLM(config)
 
 @app.post("/landmarks3d")
 def get_3d_landmarks(item: Item):
-    path_in = item.path_in
-    path_out = item.path_out
-    landmarks = dm.predict_one_file(path_in)
-    pd.DataFrame(landmarks, columns=["x", "y", "z"]).to_csv(path_out, index=False, header=True)
-    return {"status": "ok"}
+    landmarks = dm.predict_one_file(item.path_in)
+    landmarks = landmarks.tolist()
+    return Landmarks(landmarks=landmarks)
