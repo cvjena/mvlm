9342a70a44b937dd172b973c4e90819e7e819316
Tim Büchner <tim.buechner@uni-jena.de>
2024-02-04 13:08:38 +0100

    small code clean up

--- deepmvlm/api.py
+++ deepmvlm/api.py
@@ -1,14 +1,14 @@
-from pathlib import Path
 import time
+from pathlib import Path
+
 import numpy as np
 import torch
-import model.model as module_arch
-from utils3d import Utils3D
-from utils3d import Render3D
-from prediction import Predict2D
-from torch.utils.model_zoo import load_url
-# import os
 from PIL import Image
+from torch.utils.model_zoo import load_url
+
+import model.model as module_arch
+from prediction import Predict2D
+from utils3d import Render3D, Utils3D
 
 models_urls = {
     'MVLMModel_DTU3D-RGB':
@@ -77,22 +77,16 @@ class DeepMVLM:
     def _prepare_device(self, n_gpu_use):
         n_gpu = torch.cuda.device_count()
         if n_gpu_use > 0 and n_gpu == 0:
-            # self.logger.warning("Warning: There\'s no GPU available on this machine,"
-                                # "prediction will be performed on CPU.")
             n_gpu_use = 0
         if n_gpu_use > n_gpu:
-            # self.logger.warning("Warning: The number of GPU\'s configured to use is {}, but only {} are available on this machine.".format(n_gpu_use, n_gpu))
             n_gpu_use = n_gpu
         if n_gpu_use > 0 and torch.cuda.is_available() and (torch.cuda.get_device_capability()[0] * 10 + torch.cuda.get_device_capability()[1] < 35):
-            # self.logger.warning("Warning: The GPU has lower CUDA capabilities than the required 3.5 - using CPU")
             n_gpu_use = 0
         device = torch.device('cuda:0' if n_gpu_use > 0 else 'cpu')
         list_ids = list(range(n_gpu_use))
         return device, list_ids
 
     def _get_device_and_load_model_from_url(self):
-        # logger = self.config.get_logger('test')
-
         print('Initialising model')
         model = self.config.initialize('arch', module_arch)
 
@@ -105,9 +99,6 @@ class DeepMVLM:
 
         print('Getting device')
         device, device_ids = self._prepare_device(self.config['n_gpu'])
-
-        # logger.info('Loading checkpoint: {}'.format(check_point_name))
-
         checkpoint = load_url(check_point_name, model_dir, map_location=device)
 
         # Write clean model - should only be done once for translation of models
@@ -126,59 +117,7 @@ class DeepMVLM:
             model = torch.nn.DataParallel(model, device_ids=device_ids)
 
         model.load_state_dict(state_dict)
-
-
         model = model.to(device)
-        model.eval()
-        return device, model
-
-    # Deprecated - should not be used
-    def _get_device_and_load_model(self):
-        # logger = self.config.get_logger('test')
-
-        print('Initialising model')
-        model = self.config.initialize('arch', module_arch)
-        # logger.info(model)
-
-        print('Loading checkpoint')
-        model_name = self.config['name']
-        image_channels = self.config['data_loader']['args']['image_channels']
-        if model_name == "MVLMModel_DTU3D":
-            if image_channels == "geometry":
-                check_point_name = 'saved/trained/MVLMModel_DTU3D_geometry.pth'
-            elif image_channels == "RGB":
-                check_point_name = 'saved/trained/MVLMModel_DTU3D_RGB_07092019.pth'
-            elif image_channels == "depth":
-                check_point_name = 'saved/trained/MVLMModel_DTU3D_Depth_19092019.pth'
-            elif image_channels == "RGB+depth":
-                check_point_name = 'saved/trained/MVLMModel_DTU3D_RGB+depth_20092019.pth'
-            else:
-                print('No model trained for ', model_name, ' with channels ', image_channels)
-                return None, None
-        elif model_name == 'MVLMModel_BU_3DFE':
-            if image_channels == "RGB":
-                check_point_name = 'saved/trained/MVLMModel_BU_3DFE_RGB_24092019_6epoch.pth'
-            else:
-                print('No model trained for ', model_name, ' with channels ', image_channels)
-                return None, None
-        else:
-            print('No model trained for ', model_name)
-            return None
-
-        # logger.info('Loading checkpoint: {}'.format(check_point_name))
-
-        device, device_ids = self._prepare_device(self.config['n_gpu'])
-
-
-        checkpoint = torch.load(check_point_name, map_location=device)
-        state_dict = checkpoint['state_dict']
-        if len(device_ids) > 1:
-            model = torch.nn.DataParallel(model, device_ids=device_ids)
-
-        model.load_state_dict(state_dict)
-
-        model = model.to(device)
-        model = torch.compile(model)
         model.eval()
         return device, model
 
